ffeef7d64fccac3158f3ca49535e2e0e9632499e
smartadpole <smartadpole@gmail.com>
2025-01-14 14:30:52 +0800

    support onnx test

--- export_onnx/convert.py
+++ export_onnx/convert.py
@@ -4,10 +4,10 @@ import numpy as np
 import os
 import time
 import argparse
-from tools.file import MkdirSimple
-from tools.file import ReadImageList
+from utils.file import MkdirSimple
+from utils.file import ReadImageList
 from depth_anything_v2.dpt import DepthAnythingV2
-from onnxmodel import ONNXModel
+from export_onnx.onnx_test import test_dir
 
 model_configs = {
     'vits': {'encoder': 'vits', 'features': 64, 'out_channels': [48, 96, 192, 384]},
@@ -27,6 +27,7 @@ def parse_args():
     parser.add_argument("--device", type=str, default=torch.device("cuda" if torch.cuda.is_available() else "cpu"), help="Device to run the model on.")
     parser.add_argument("--width", type=int, default=W, help="Width of the input image.")
     parser.add_argument("--height", type=int, default=H, help="Height of the input image.")
+    parser.add_argument("--test", action="store_true", help="test model")
     return parser.parse_args()
 
 
@@ -55,52 +56,18 @@ def export_to_onnx(model_path, onnx_file, width=W, height=H, device="cuda"):
 
     print(f"Model exported to {onnx_file}")
 
-
-def test_onnx(img_path, model, width=W, height=H):
-    img_org = cv2.imread(img_path)
-    img = cv2.resize(img_org, (width, height), cv2.INTER_LANCZOS4)
-    mean=[0.485, 0.456, 0.406]
-    std=[0.229, 0.224, 0.225]
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) / 255.0
-    img = np.subtract(img, mean)
-    img = np.divide(img, std)
-    img = img.transpose(2, 0, 1)
-    img = np.expand_dims(img, axis=0).astype("float32")
-
-    start_time = time.time()
-    output = model.forward(img)
-    end_time = time.time()
-    print(f"Execution time: {(end_time - start_time) * 1000:.2f} ms")
-    dis_array = output[0][0]
-    dis_array = (dis_array - dis_array.min()) / (dis_array.max() - dis_array.min()) * 255.0
-    dis_array = dis_array.astype("uint8")
-
-    depth = cv2.resize(dis_array, (img_org.shape[1], img_org.shape[0]))
-    depth = cv2.applyColorMap(cv2.convertScaleAbs(depth, 1), cv2.COLORMAP_PARULA)
-    combined_img = np.vstack((img_org, depth))
-
-    return combined_img, depth
-
-def test_dir(image_dir, model_file, output_dir, width, height):
-    model = ONNXModel(model_file)
-    img_list = ReadImageList(image_dir)
-    print("test image number: ", len(img_list))
-    for file in img_list:
-        image, depth = test_onnx(file, model, width, height)
-        depth_file = os.path.join(output_dir, 'depth', os.path.basename(file))
-        concat_file = os.path.join(output_dir, 'concat', os.path.basename(file))
-        MkdirSimple(depth_file)
-        MkdirSimple(concat_file)
-        cv2.imwrite(concat_file, image)
-        cv2.imwrite(depth_file, depth)
-
 def main():
     args = parse_args()
-    output_dir = os.path.join(args.output, f"{args.width}_{args.height}")
-    onnx_file = os.path.join(output_dir, os.path.splitext(os.path.basename(args.checkpoint))[0] + ".onnx")
-    MkdirSimple(onnx_file)
+    model_name = os.path.splitext(os.path.basename(args.checkpoint))[0].replace(" ", "_")
+    output = os.path.join(args.output, model_name, f'{args.width}_{args.height}')
+    onnx_file = os.path.join(output, f'DepthAnythingV2_{args.width}_{args.height}_{model_name}_12.onnx')
+    MkdirSimple(output)
+
     export_to_onnx(args.checkpoint, onnx_file, args.width, args.height, args.device)  # Replace 'vitl' with the desired encoder
-    test_dir(args.image, onnx_file, output_dir, args.width, args.height)
+
+    print("export onnx to {}".format(onnx_file))
+    if args.test:
+        test_dir(onnx_file, [args.image, ], output)
 
 
 if __name__ == "__main__":
